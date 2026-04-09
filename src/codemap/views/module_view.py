611249f38743374d0071979e5bc842eec4e75009
haddyadnan <haddyadnan@gmail.com>
2026-04-09 21:53:09 +0100

    add module view

---
+++ src/codemap/views/module_view.py
@@ -0,0 +1,16 @@
+from codemap.errors import ModuleViewError
+from codemap.graph.models import GraphBundle
+from codemap.views.subgraph import build_function_subgraph
+
+
+def module_function_view(bundle: GraphBundle, module_id: str) -> GraphBundle:
+    _validate_module(bundle, module_id)
+    function_ids = {
+        function.id for function in bundle.functions if function.module_id == module_id
+    }
+    return build_function_subgraph(bundle, function_ids)
+
+
+def _validate_module(bundle: GraphBundle, module_id: str) -> None:
+    if bundle.module_by_id(module_id) is None:
+        raise ModuleViewError(f"Module not found: {module_id}")
