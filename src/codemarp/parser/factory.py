5ecd4a13edc7cfc9757715386b82b68de10f24e8
haddyadnan <haddyadnan@gmail.com>
2026-04-19 08:31:24 +0100

    rename variable

--- src/codemarp/parser/factory.py
+++ src/codemarp/parser/factory.py
@@ -5,7 +5,7 @@ from codemarp.parser.python.tree_sitter_parser import TreeSitterPythonParser
 
 def get_parser(language: str, module_id: str, engine: str = "ast") -> LanguageParser:
     if language == "python":
-        if engine == "tree_sitter":
+        if engine == "tree-sitter":
             return TreeSitterPythonParser(module_id)
         return PythonParser(module_id)
 
