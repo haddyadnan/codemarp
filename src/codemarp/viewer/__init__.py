340e70b2019655d1fafea1240e7aa7098f736930
haddyadnan <haddyadnan@gmail.com>
2026-04-27 23:19:49 +0100

    add cytoscape viewer

--- src/codemarp/viewer/__init__.py
+++ src/codemarp/viewer/__init__.py
@@ -1,3 +1,7 @@
-from codemarp.viewer.html import open_mermaid_view, wrap_mermaid_html
+from codemarp.viewer.html import (
+    open_mermaid_view,
+    wrap_cytoscape_html,
+    wrap_mermaid_html,
+)
 
-__all__ = ["open_mermaid_view", "wrap_mermaid_html"]
+__all__ = ["open_mermaid_view", "wrap_mermaid_html", "wrap_cytoscape_html"]
