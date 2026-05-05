0033f5eb2321d27337cab7d44bcfe4536ccc7401
haddyadnan <haddyadnan@gmail.com>
2026-05-06 00:42:03 +0100

    delete sample file

--- src/codemarp/parser/js_parser.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-class JavaScriptParser:
-    def parse_file(self, root: Path, path: Path) -> dict:
-        raise NotImplementedError("JavaScript parsing will come in a later phase.")
