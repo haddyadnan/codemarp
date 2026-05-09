79a38daabc094ecf5672de8748e41e11ae9214ea
haddyadnan <haddyadnan@gmail.com>
2026-05-09 14:31:37 +0100

    update tests

--- tests/javascript/test_js_functions.py
+++ tests/javascript/test_js_functions.py
@@ -39,3 +39,84 @@ const fn = function () {};
     ).parse_code_to_facts(code)
 
     assert {fn.function_id for fn in parsed.functions} == {"app.main:fn"}
+
+
+def test_tree_sitter_javascript_extracts_property_assigned_function() -> None:
+    code = """
+exports.createApplication = function () {
+  init();
+};
+
+Router.prototype.handle = function () {
+  dispatch();
+};
+
+app.use = () => {
+  mount();
+};
+"""
+
+    parsed = TreeSitterTypeScriptParser(
+        "express",
+        language="javascript",
+    ).parse_code_to_facts(code)
+
+    assert {fn.function_id for fn in parsed.functions} == {
+        "express:exports.createApplication",
+        "express:Router.prototype.handle",
+        "express:app.use",
+    }
+
+    calls = {(c.caller_id, c.raw) for c in parsed.calls}
+
+    assert ("express:exports.createApplication", "init") in calls
+    assert ("express:Router.prototype.handle", "dispatch") in calls
+    assert ("express:app.use", "mount") in calls
+
+
+def test_tree_sitter_javascript_ignores_dynamic_property_assignment() -> None:
+    code = """
+obj[name] = function () {};
+obj[prefix + key] = () => {};
+factory().method = function () {};
+"""
+
+    parsed = TreeSitterTypeScriptParser(
+        "app",
+        language="javascript",
+    ).parse_code_to_facts(code)
+
+    assert parsed.functions == []
+
+
+def test_tree_sitter_javascript_normalizes_module_exports_property_assignment() -> None:
+    code = """
+module.exports.createApplication = function () {};
+"""
+
+    parsed = TreeSitterTypeScriptParser(
+        "app",
+        language="javascript",
+    ).parse_code_to_facts(code)
+
+    assert {fn.function_id for fn in parsed.functions} == {
+        "app:exports.createApplication",
+    }
+
+
+def test_tree_sitter_javascript_keeps_regular_and_property_functions_distinct() -> None:
+    code = """
+function run() {}
+
+exports.run = function () {};
+"""
+
+    parsed = TreeSitterTypeScriptParser(
+        "app",
+        language="javascript",
+    ).parse_code_to_facts(code)
+
+    assert {fn.function_id for fn in parsed.functions} == {
+        "app:run",
+        "app:exports.run",
+    }
